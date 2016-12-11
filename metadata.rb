081f9f83aa5b465e64881ee7042cad3fefd5b2eb
Byron Schaller <byron.schaller@gmail.com>
2016-12-11 15:58:58 -0500

    added yum repo resource and tests

--- metadata.rb
+++ metadata.rb
@@ -5,11 +5,7 @@ license 'all_rights'
 description 'Installs/Configures grafana'
 long_description 'Installs/Configures grafana'
 version '0.1.0'
-
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Issues` link
-# issues_url 'https://github.com/<insert_org_here>/grafana/issues' if respond_to?(:issues_url)
-
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Source` link
-# source_url 'https://github.com/<insert_org_here>/grafana' if respond_to?(:source_url)
+source_url 'https://github.com/byronschaller/grafana'
+issues_url 'https://github.com/byronschaller/grafana/issues'
+chef_version "~> 12.14"
+supports "centos", "~> 7"
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,15 @@
 # Cookbook Name:: grafana
 # Recipe:: default
 #
-# Copyright (c) 2016 The Authors, All Rights Reserved.
+# Copyright (c) 2016 Byron Schaller, All Rights Reserved.
+
+yum_repository 'grafana' do
+  description "Grafana Stable repo"
+  baseurl "https://packagecloud.io/grafana/stable/el/6/$basearch"
+  repo_gpgcheck true
+  enabled true
+  gpgcheck true
+  gpgkey "https://packagecloud.io/gpg.key https://grafanarel.s3.amazonaws.com/RPM-GPG-KEY-grafana"
+  sslverify true
+  sslcacert "/etc/pki/tls/certs/ca-bundle.crt"
+end
